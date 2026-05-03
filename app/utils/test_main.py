a0bea1154891427577b75bcfdb698f22ffe1b621
Thiago Tolotti <thiagotolotti@gmail.com>
2026-05-03 19:33:45 -0300

    adds tests for utils/main

---
+++ app/utils/test_main.py
@@ -0,0 +1,10 @@
+from app.utils import settings
+
+
+def test_settings():
+    assert settings is not None
+    assert settings.private_key is not None
+    assert settings.public_key is not None
+
+    assert "BEGIN PRIVATE KEY" in settings.private_key
+    assert "BEGIN PUBLIC KEY" in settings.public_key
