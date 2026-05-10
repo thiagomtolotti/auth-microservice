aa8e3152787bbe61c6c3412e943f8b0d6ed7a0e3
Thiago Tolotti <thiagotolotti@gmail.com>
2026-05-10 00:19:27 -0300

    adds wrong pass login test

--- tests/integration/test_login.py
+++ tests/integration/test_login.py
@@ -27,3 +27,17 @@ def test_no_user_login(client: TestClient):
 
     assert json_response["type"] == "LoginFailedException"
     assert json_response["detail"] == "Invalid email or password"
+
+
+def test_wrong_password_login(client: TestClient):
+
+    create_user(client, TEST_EMAIL, TEST_PASSWORD)
+
+    response = login(client, TEST_EMAIL, "wrong_password")
+
+    assert response.status_code == 400
+
+    json_response = response.json()
+
+    assert json_response["type"] == "LoginFailedException"
+    assert json_response["detail"] == "Invalid email or password"
