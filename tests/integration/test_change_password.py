c514b1fe142b47b4f18d0b9a08b3e71f34f88d27
Thiago Tolotti <thiagotolotti@gmail.com>
2026-05-10 10:34:13 -0300

    adds more tests for change password request

--- tests/integration/test_change_password.py
+++ tests/integration/test_change_password.py
@@ -29,3 +29,29 @@ def test_change_password(client: TestClient):
     login_res = login(client, TEST_EMAIL, new_password)
 
     assert login_res.status_code == 200
+
+
+def test_change_password_unauthorized(client: TestClient):
+    response = change_password(client, "invalid_token", "@newpassword123")
+
+    assert response.status_code == 401
+
+
+def test_change_password_invalid_input(client: TestClient):
+    login_res = register_and_login(client, TEST_EMAIL, TEST_PASSWORD)
+
+    json = login_res.json()
+
+    access_token = json["access_token"]
+
+    response = change_password(client, access_token, "")
+    assert response.status_code == 422
+
+    response = change_password(client, access_token, "@short1")
+    assert response.status_code == 422
+
+    response = change_password(client, access_token, "@nonumbers")
+    assert response.status_code == 422
+
+    response = change_password(client, access_token, "nospecialchars123")
+    assert response.status_code == 422
