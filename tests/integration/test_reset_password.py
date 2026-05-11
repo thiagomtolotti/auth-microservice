b8993599fcbd833f77961668dfaf12baf8492582
Thiago Tolotti <thiagotolotti@gmail.com>
2026-05-10 23:46:32 -0300

    adds more tests for reset_password function

--- tests/integration/test_reset_password.py
+++ tests/integration/test_reset_password.py
@@ -44,3 +44,30 @@ def test_reset_password_with_expired_token(
     reset_password_res = reset_password(client, TEST_EMAIL, token, "@newpassword123")
 
     assert reset_password_res.status_code == 422
+
+
+def test_reset_password_with_same_password(
+    client: TestClient, notification_handler: MockNotificationHandler
+):
+    create_user(client, TEST_EMAIL, TEST_PASSWORD)
+
+    forgot_password(client, TEST_EMAIL)
+
+    token = notification_handler.forgot_password_calls[0].token
+
+    reset_password_res = reset_password(client, TEST_EMAIL, token, TEST_PASSWORD)
+
+    assert reset_password_res.status_code == 422
+
+
+def test_reset_password_with_nonexistent_email(client: TestClient):
+    reset_password_res = reset_password(
+        client, "nonexistent@example.com", "any-token", "@newpassword123"
+    )
+
+    json = reset_password_res.json()
+
+    assert reset_password_res.status_code == 400
+
+    assert json["type"] == "UserNotFoundException"
+    assert json["detail"] == "User not found"
