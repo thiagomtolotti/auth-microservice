8f899fc3cacc73b3f4030e7c4dc9734899f6f172
Thiago Tolotti <thiagotolott@gmail.com>
2026-05-13 15:50:45 -0300

    renames integration tests for default router to default routes

--- tests/integration/test_default_routes.py
+++ tests/integration/test_default_routes.py
@@ -1,11 +1,11 @@
-from fastapi.testclient import TestClient
 import pytest
+
+from fastapi.testclient import TestClient
 
 from app.routes import Routes
 
-from .constants import TEST_EMAIL, TEST_PASSWORD
-
 from .flows import get_route, register_and_login, delete_user, access_protected
+
 
 
 def test_ping(client: TestClient):
@@ -21,7 +21,7 @@ def test_protected(client: TestClient):
 
 
 def test_protected_with_token(client: TestClient):
-    login = register_and_login(client, TEST_EMAIL, TEST_PASSWORD)
+    login = register_and_login(client)
 
     json = login.json()
     access_token = json["access_token"]
@@ -41,7 +41,7 @@ def test_protected_with_expired_token(
 ):
     monkeypatch.setattr("app.domain.vos.tokens.ACCESS_TOKEN_DURATION", -1)
 
-    login = register_and_login(client, TEST_EMAIL, TEST_PASSWORD)
+    login = register_and_login(client)
     json = login.json()
     access_token = json["access_token"]
 
@@ -51,7 +51,7 @@ def test_protected_with_expired_token(
 
 
 def test_protected_refresh_token(client: TestClient):
-    login = register_and_login(client, TEST_EMAIL, TEST_PASSWORD)
+    login = register_and_login(client)
     json = login.json()
     refresh_token = json["refresh_token"]
 
@@ -64,9 +64,10 @@ def test_protected_refresh_token(client: TestClient):
 
 
 def test_protected_deleted_user(client: TestClient):
-    login = register_and_login(client, TEST_EMAIL, TEST_PASSWORD)
+    login = register_and_login(client)
     json = login.json()
     access_token = json["access_token"]
+    
 
     delete_user(client, access_token)
 
