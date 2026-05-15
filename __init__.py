b2ec42062d83668cec79bf66eb44ebfdeeb7fd01
Thiago Tolotti <thiagotolotti@gmail.com>
2026-05-15 14:00:20 -0300

    initialy adds uv

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
-from .app.main import setup, UsersService, AuthNotificationHandler
-from .app.repositories.users import UsersRepository
-from .app.routes import Routes
+from app.main import setup, UsersService, AuthNotificationHandler
+from app.repositories.users import UsersRepository
+from app.routes import Routes
 
 __all__ = [
-    "setup", 
-    "AuthNotificationHandler", 
-    "UsersService", 
+    "setup",
+    "AuthNotificationHandler",
+    "UsersService",
     "UsersRepository",
     "Routes",
 ]
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from auth-microservice!")
+
+
+if __name__ == "__main__":
+    main()
